9637131383726c965bac818101628652c3027f08
John Toomey <john@toganlabs.com>
2020-10-21 17:21:30 +0000

    Changes to the release script from first release
    Signed-off-by: John Toomey <john@toganlabs.com>

--- source_release.sh
+++ source_release.sh
@@ -4,17 +4,16 @@ set -x
 
 function error { echo "$1" && exit 1; }
 
-VERSION="v0.9"
-GIT_BRANCH="dunfell"
+VERSION="0.9" #dunfell
 GH_SERVER="git@github.com:APS-Networks"
-GIT_CLONE="git clone --branch ${GIT_BRANCH}"
+GIT_CLONE="git clone --branch ${VERSION}"
 
 [ -d "mion" ] && error "mion/ exists - delete and re-run"
 
-${GIT_CLONE} --recursive "${GH_SERVER}/mion.git"
-${GIT_CLONE} "${GH_SERVER}/meta-mion.git" "mion/meta-mion"
-${GIT_CLONE} "${GH_SERVER}/meta-mion-stordis.git" "mion/meta-mion-stordis"
+${GIT_CLONE} --recursive "${GH_SERVER}/mion.git" "mion_${VERSION}"
+${GIT_CLONE} "${GH_SERVER}/meta-mion.git" "mion_${VERSION}/meta-mion"
+${GIT_CLONE} "${GH_SERVER}/meta-mion-stordis.git" "mion_${VERSION}/meta-mion-stordis"
 
-tar --exclude=".*" -cJf "mion_${VERSION}.tar.xz" "mion/"
+tar --exclude=".git*" -czf "mion_${VERSION}.tar.gz" "mion_${VERSION}/"
 
-rm -rf "mion"
+rm -rf "mion_${VERSION}"
