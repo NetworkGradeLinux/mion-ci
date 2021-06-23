b6c924eedaf9cf1dfcc51dc197587d3e989531ee
John Toomey <john@toganlabs.com>
2021-06-23 11:37:56 +0100

    Update changelog script
    Signed-off-by: John Toomey <john@toganlabs.com>

--- gen_changelog.sh
+++ gen_changelog.sh
@@ -1,27 +1,40 @@
-#!/bin/bash
+#!/bin/sh
+set -e
 
 LAST_RELEASE="dunfell-2021.03"
 CHANGELOG="changelog.txt"
 
-repos=(mion meta-mion meta-mion-bsp meta-mion-backports meta-mion-sde mion-docs)
+PRETTY_FORMAT="%h -%d %s (%cr) %an"
+
+[ "$1" = "-r" ] && PRETTY_FORMAT="- %h %s"
+
+repos="
+    mion
+    meta-mion
+    meta-mion-backports
+    meta-mion-bsp
+    meta-mion-sde
+    mion-docs
+    mion-testing
+"
 
 [ -f ${CHANGELOG} ] && rm ${CHANGELOG}
 
-for r in "${repos[@]}"; do
+for r in ${repos}; do
     [ ! -d "$r" ] && git clone "git@github.com:NetworkGradeLinux/${r}.git"
     
     (
-        printf "> %s\n" "$r"
+        printf "> %s\\n" "$r"
         cd "$r" && git pull > /dev/null 2>&1
 
-        printf "*** %s ***\n\n" "$r" >> ../${CHANGELOG}
+        printf "*** %s ***\\n\\n" "$r" >> ../${CHANGELOG}
 
         if git rev-parse ${LAST_RELEASE} >/dev/null 2>&1; then
-            git log ${LAST_RELEASE}..HEAD --pretty=format:"%h - %d %s (%cr) %an" >> ../${CHANGELOG}
+            git log ${LAST_RELEASE}..HEAD --pretty=format:"${PRETTY_FORMAT}" >> ../${CHANGELOG}
         else
-            git log --pretty=format:"%h - %d %s (%cr) %an" >> ../${CHANGELOG}
+            git log --pretty=format:"${PRETTY_FORMAT}" >> ../${CHANGELOG}
         fi
 
-        printf "\n\n" >> ../${CHANGELOG}
+        printf "\\n\\n" >> ../${CHANGELOG}
     )
 done
